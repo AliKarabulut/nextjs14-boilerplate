6b6571a928539954e8f82ee3157b4d3c809a686d
Cem <cemtatli3535@gmail.com>
2024-01-28 17:18:44 +0300

    update: environment variables and metadata URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('http:localhost:3000'),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'),
   robots: {
     index: true,
     follow: true,
