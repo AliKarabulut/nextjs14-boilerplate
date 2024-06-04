f4c7fb076a5951b895239ee1a12c5ba1b84408c4
alikarabulut <a.kbt6666@gmail.com>
2024-06-04 03:09:11 +0300

    feat: added dynmic font sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
 const Home = () => {
-  return <main>Home</main>
+  return (
+    <main>
+      <div className="text-dynmic-xs">Home</div>
+      <div className="text-dynmic-sm">Home</div>
+      <div className="text-dynmic-base">Home</div>
+      <div className="text-dynmic-lg">Home</div>
+      <div className="text-dynmic-xl">Home</div>
+      <div className="text-dynmic-2xl">Home</div>
+      <div className="text-dynmic-3xl">Home</div>
+      <div className="text-dynmic-4xl">Home</div>
+      <div className="text-dynmic-5xl">Home</div>
+      <div className="text-dynmic-6xl">Home</div>
+      <div className="text-dynmic-7xl">Home</div>
+      <div className="text-dynmic-8xl">Home</div>
+      <div className="text-dynmic-9xl">Home</div>
+    </main>
+  )
 }
 export default Home
