cda9e28e7854e5a1d5a7d92763bfa28393c59f02
alikarabulut <a.kbt6666@gmail.com>
2024-06-14 03:46:01 +0300

    feat: added prettier config for staged

---
+++ .prettierrc-staged.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: false,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 140,
+    endOfLine: 'auto',
+    plugins: ['prettier-plugin-tailwindcss', "prettier-plugin-organize-imports"],
+    organizeImportsSkipDestructiveCodeActions: false
+}
